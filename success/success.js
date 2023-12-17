49da83465dd6ffcc3ccdfe4f0becdd9f3a4a9c44
geeteshmehuria <geeteshmaihuria@gmail.com>
2023-12-17 18:40:45 +0530

    Login&Signup

--- success/success.js
+++ success/success.js
@@ -3,10 +3,10 @@ let container = document.getElementById("bookingDetails")
 let create = (element) => document.createElement(element)
 
 function loadEventData(){ 
-  //  let bookingDetails=JSON.parse(localStorage.getItem("bookingDeatails"));
-//console.log(bookingDetails);
+   let bookingDetails=JSON.parse(localStorage.getItem("bookingDeatails"));
+console.log(bookingDetails);
 
-let bookingDetails={"noOfPeople":"3","date":"2023-12-20","totalPrice":150}
+// let bookingDetails={"noOfPeople":"3","date":"2023-12-20","totalPrice":150}
     let event = JSON.parse(localStorage.getItem("event"))
     
     let eventTitle = document.getElementById("eventTitle");
